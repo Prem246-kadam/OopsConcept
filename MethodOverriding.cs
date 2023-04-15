2e94bbc14af4f6f8b3f3a082212f21f9813a4184
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-04-15 08:25:50 +0530

    Add files via upload

---
+++ MethodOverriding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept.Polymorphism
+{
+    class Animal
+    {
+        public string name;
+        public virtual void Run()
+        {
+            Console.WriteLine("running slow");
+
+        }
+        public void AnimalDetails()
+        {
+            Console.WriteLine("Name of animal:" + name);
+                }
+
+    }
+    class lion:Animal
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Running Faster");
+                
+        }
+    }
+}
---
+++ PartTimeEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept.Abstraction
+{
+   class PartTimeEmployee : Employee
+    {
+        public override void GetSalary()
+        {
+            Console.WriteLine("50000");
+        }
+    }
+}
