7845c548e46a2fff28ce491951ab25568661b7d2
Prem kadam <premkadam246@gmail.com>
2023-04-16 16:01:52 +0530

    Adding Oops files

---
+++ OppsConcept/Encapsulation/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept.Encapsulation
+{
+     class Student
+    {
+        private string studentName;
+
+        public string Name
+        {
+            get { return studentName; }
+            set { studentName = value; }
+
+        }
+        
+    }
+}
---
+++ OppsConcept/Inheritance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConcept
+{
+    internal class Vehicle
+    {
+        public string brand;
+
+        public void Start() 
+        {
+            Console.WriteLine("Vehicle started");
+        }
+    }
+}
